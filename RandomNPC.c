6fefe07f6032c961fe3b4acdea3a22733215f39c
Gabriele <gabriele.lavorato@hotmail.it>
2019-12-09 21:25:35 +0100

    races and gender added

--- RandomNPC.c
+++ RandomNPC.c
@@ -13,7 +13,7 @@
 #define NUMBER_OF_APPEREANCES 26
 #define NUMBER_OF_TALENTS 20
 #define NUMBER_OF_ORIGINS 3
-#define NUMBER_OF_RACES 5
+#define NUMBER_OF_RACES 11
 
 #define BUFF_SIZE_SHORT 32
 #define BUFF_SIZE_LONG 256
@@ -26,11 +26,22 @@
   * This program randomly generates a detailed NPC
   */
 
-
+struct mynpc{
+    char name[BUFF_SIZE_SHORT];
+    char surname[BUFF_SIZE_SHORT];
+    char race[BUFF_SIZE_SHORT];
+    char appearance[BUFF_SIZE_LONG];
+    char abilities[BUFF_SIZE_LONG];
+    char talents[BUFF_SIZE_LONG];
+    char origins[BUFF_SIZE_LONG];
+    char gender[BUFF_SIZE_SHORT];
+} npc;
 
 static char* pickName();
 
 static char* pickSurname();
+
+static char* pickGender();
 
 static char* pickOrigins();
 
@@ -40,31 +51,29 @@ static char* pickAppearance();
 
 static char* pickAbilities();
 
+static char* pickRace();
+
 static int randomizer(int number);
 
 
 int main() {
 
     printf("This is a random NPC generator\n");
-    
-    char res[30];
-    strcpy(res, pickName());
-    printf("The name is: %s\n", res);
 
-    strcpy(res, pickSurname());
-    printf("The surname is: %s\n", res);
+    strcpy(npc.name, pickName());
+    strcpy(npc.surname, pickSurname());
+    strcpy(npc.race, pickRace());
+    strcpy(npc.talents, pickTalent());
+    strcpy(npc.abilities, pickAbilities());
+    strcpy(npc.origins, pickOrigins());
+    strcpy(npc.appearance, pickAppearance());
+    strcpy(npc.gender, pickGender());
 
-    strcpy(res, pickTalent());
-    printf("The talent is: %s\n", res);
-
-    strcpy(res, pickAbilities());
-    printf("The abilities is: %s\n", res);
-
-    strcpy(res, pickOrigins());
-    printf("The origin is: %s\n", res);
-
-    strcpy(res, pickAppearance());
-    printf("The appereance is: %s\n", res);
+    printf("You meet a %s %s named %s %s.\n", npc.gender, npc.race, npc.name, npc.surname);
+    printf("%s seems capable of %s.\n", npc.name, npc.talents);
+    printf("The abilities is: %s\n", npc.abilities);
+    printf("The origin is: %s\n", npc.origins);
+    printf("The appereance is: %s\n", npc.appearance);
 
     printf("--- End of program ---\n");
     printf("(press any key to exit)");
@@ -219,8 +228,6 @@ static char* pickTalent() {
     char line[BUFF_SIZE_LONG]; 
     for (count = 0; (fgets(local_talent, sizeof local_talent, fp_talent) != NULL); count++) {
         if (count == randomLine) {
-            // fscanf(fp_talent, "%s", &local_talent);
-            // fputs(local_talent, stdout);
             strtok(local_talent, "\n");
             strcpy(talent, local_talent);
         } 
@@ -228,6 +235,43 @@ static char* pickTalent() {
 
     fclose(fp_talent);
     return talent;
+}
+
+static char* pickRace() {
+    FILE *fp_race = fopen("./sources/races.txt", "r");
+    char *race = malloc(BUFF_SIZE_LONG * sizeof(char));
+    char local_race[BUFF_SIZE_LONG];
+    int randomLine = randomizer(NUMBER_OF_TALENTS);
+
+    if (!fp_race) {
+        fprintf(stderr, "%s: line %d. Error #%d opening file races.txt: %s.\n", 
+        __FILE__, __LINE__, errno, strerror(errno));
+        getchar();
+        exit(EXIT_FAILURE);
+    }
+    
+    int count = 0;
+    char line[BUFF_SIZE_LONG]; 
+    for (count = 0; (fgets(local_race, sizeof local_race, fp_race) != NULL); count++) {
+        if (count == randomLine) {
+            strtok(local_race, "\n");
+            strcpy(race, local_race);
+        } 
+    }
+
+    fclose(fp_race);
+    return race;
+}
+
+static char * pickGender() {
+    char *gender = malloc(BUFF_SIZE_SHORT * sizeof(char));
+    int _gender = randomizer(1);
+    if (_gender) {
+        strcpy(gender, "male");
+    } else {
+        strcpy(gender, "female");
+    }
+    return gender;
 }
 
 static int randomizer(int max_value) {
