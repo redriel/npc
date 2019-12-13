7fbda3b4ececd020050915eabf5582e69550e4e4
Gabriele Lavorato <csto1151@consoft.it>
2019-12-13 14:46:09 +0100

    merge conflict resolved

--- npc.c
+++ npc.c
@@ -1,27 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <errno.h>
-<<<<<<< HEAD
-#include <unistd.h>
-#include <unistd.h>
-#include <time.h>
-
-// To get the number to insert, just subtract 2 from the number of file lines
-#define NUMBER_OF_NAMES 198
-#define NUMBER_OF_SURNAMES 973
-#define NUMBER_OF_ABILITIES 12
-#define NUMBER_OF_APPEREANCES 23
-#define NUMBER_OF_TALENTS 21
-#define NUMBER_OF_ORIGINS 20
-#define NUMBER_OF_RACES 11
-#define NUMBER_OF_BEHAVIOURS 11
-
-#define BUFF_SIZE_SHORT 32
-#define BUFF_SIZE_LONG 256
-=======
 #include "lib/npcutils.h"
 #include "lib/npcgenerator.h"
->>>>>>> develop
 
 /**
   *
