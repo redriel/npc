329b000ea4de56ef8442dc994a8fcc1c582c336e
Gabriele Lavorato <csto1151@consoft.it>
2019-12-13 10:29:55 +0100

    race and gender options added as inline arguments

--- lib/npcutils.c
+++ lib/npcutils.c
@@ -33,19 +33,19 @@ int file_length(char * file_path) {
 }
 
 struct npc generateNPC(int length, char * arguments[]) {
-    int code = argv_validator(length, arguments); 
+    struct npc npc;
+    int code = argv_validator(length, arguments);
     if (code > 0) {
-        if (code == 1) {
-            struct npc npc;
+        if (code >= 1) {
             strcpy(npc.name, pickLine(NAMES_PATH));
             strcpy(npc.surname, pickLine(SURNAMES_PATH));
-            strcpy(npc.race, arguments[3]);
+            strcpy(npc.race, pickLine(RACES_PATH));
             strcpy(npc.abilities, pickLine(ABILITIES_PATH));
             strcpy(npc.appearance, pickLine(APPEARANCES_PATH));
             strcpy(npc.talents, pickLine(TALENTS_PATH));
             strcpy(npc.origins, pickLine(ORIGINS_PATH));
             strcpy(npc.behaviour, pickLine(BEHAVIOURS_PATH));
-            if(strcmp(arguments[2], "male") == 0) {
+            if(pickGender()) {
                 strcpy(npc.gender, "male");
                 strcpy(npc.pronoun_upper, "He");
                 strcpy(npc.pronoun_lower, "he");
@@ -53,72 +53,31 @@ struct npc generateNPC(int length, char * arguments[]) {
                 strcpy(npc.gender, "female");
                 strcpy(npc.pronoun_upper, "She");
                 strcpy(npc.pronoun_lower, "she");
+            }   
+        } if (code == 2) {
+            if(strcmp(arguments[2], "male") == 0) {
+                strcpy(npc.gender, "male");
+                strcpy(npc.pronoun_upper, "He");
+                strcpy(npc.pronoun_lower, "he");
+            } else if (strcmp(arguments[2], "female") == 0) {
+                strcpy(npc.gender, "female");
+                strcpy(npc.pronoun_upper, "She");
+                strcpy(npc.pronoun_lower, "she");
             }
-            return npc;
-        } else if (code == 2) {
-            struct npc npc;
-            strcpy(npc.name, pickLine(NAMES_PATH));
-            strcpy(npc.surname, pickLine(SURNAMES_PATH));
+        } if (code == 3) {
             strcpy(npc.race, arguments[2]);
-            strcpy(npc.abilities, pickLine(ABILITIES_PATH));
-            strcpy(npc.appearance, pickLine(APPEARANCES_PATH));
-            strcpy(npc.talents, pickLine(TALENTS_PATH));
-            strcpy(npc.origins, pickLine(ORIGINS_PATH));
-            strcpy(npc.behaviour, pickLine(BEHAVIOURS_PATH));
-            if(pickGender()) {
-                strcpy(npc.gender, "male");
-                strcpy(npc.pronoun_upper, "He");
-                strcpy(npc.pronoun_lower, "he");
-            } else {
-                strcpy(npc.gender, "female");
-                strcpy(npc.pronoun_upper, "She");
-                strcpy(npc.pronoun_lower, "she");
-            }
-            return npc;
-        } else if (code == 3) {
-            struct npc npc;
-            strcpy(npc.name, pickLine(NAMES_PATH));
-            strcpy(npc.surname, pickLine(SURNAMES_PATH));
-            strcpy(npc.race, pickLine(RACES_PATH));
-            strcpy(npc.abilities, pickLine(ABILITIES_PATH));
-            strcpy(npc.appearance, pickLine(APPEARANCES_PATH));
-            strcpy(npc.talents, pickLine(TALENTS_PATH));
-            strcpy(npc.origins, pickLine(ORIGINS_PATH));
-            strcpy(npc.behaviour, pickLine(BEHAVIOURS_PATH));
+        } if (code == 4) {
+            strcpy(npc.race, pickLine(arguments[3]));
             if(strcmp(arguments[2], "male") == 0) {
                 strcpy(npc.gender, "male");
                 strcpy(npc.pronoun_upper, "He");
                 strcpy(npc.pronoun_lower, "he");
-            } else {
+            } else if(strcmp(arguments[2], "female") == 0) {
                 strcpy(npc.gender, "female");
                 strcpy(npc.pronoun_upper, "She");
                 strcpy(npc.pronoun_lower, "she");
             }
-            return npc;
-        } else if (code == 4) {
-           struct npc npc;
-            strcpy(npc.name, pickLine(NAMES_PATH));
-            strcpy(npc.surname, pickLine(SURNAMES_PATH));
-            strcpy(npc.race, pickLine(RACES_PATH));
-            strcpy(npc.abilities, pickLine(ABILITIES_PATH));
-            strcpy(npc.appearance, pickLine(APPEARANCES_PATH));
-            strcpy(npc.talents, pickLine(TALENTS_PATH));
-            strcpy(npc.origins, pickLine(ORIGINS_PATH));
-            strcpy(npc.behaviour, pickLine(BEHAVIOURS_PATH));
-            if(pickGender()) {
-                strcpy(npc.gender, "male");
-                strcpy(npc.pronoun_upper, "He");
-                strcpy(npc.pronoun_lower, "he");
-            } else {
-                strcpy(npc.gender, "female");
-                strcpy(npc.pronoun_upper, "She");
-                strcpy(npc.pronoun_lower, "she");
-            }
-            return npc; 
-        } else {
-            message_error();
-            exit(EXIT_FAILURE);
-        }
+        } return npc;
     } else {
         message_error();
         exit(EXIT_FAILURE);
@@ -127,29 +86,26 @@ struct npc generateNPC(int length, char * arguments[]) {
 
 int argv_validator(int length, char * arguments[]) {
     int code = 0;
-    printf("here4\n");
-    if (strcmp(arguments[1], "g") == 0 && 
-    strcmp(arguments[2], "male") == 0 || //@TODO: modify the logic expression!
-    strcmp(arguments[2], "female") == 0 &&
-    race_validator(arguments[3])) {
+    if(length == 2 && (strcmp(arguments[1], "g") == 0)){
         code = 1;
-        printf("here5\n");
-    } else if (strcmp(arguments[1], "g") == 0 && 
-    race_validator(arguments[2])) {
-        code = 2;
-    } else if (strcmp(arguments[1], "g") == 0 && 
-    strcmp(arguments[2], "male") == 0 || 
-    strcmp(arguments[2], "female")) {
-        code = 3;
-    } else if (strcmp(arguments[1], "g")) {
-        code = 4;
+        return code;
+    } else if(length == 3 && (strcmp(arguments[2], "male") == 0) ||
+        length == 3 && (strcmp(arguments[2], "female") == 0)) {
+            code = 2;
+            return code;
+    } else if (length == 3 && race_validator(arguments[2]) == 1 ) {
+            code = 3;
+            return code;
+    } else if (length == 4 && (strcmp(arguments[2], "male") == 0) && race_validator(arguments[3]) == 1 || 
+        length == 4 && (strcmp(arguments[2], "female") == 0) && race_validator(arguments[3]) == 1) {
+            code = 4;
+            return code; 
+    } else {
+        return code;
     }
-    printf("code is :%d\n", code);
-    return code;
 }
 
 int race_validator(char * race) {
-    printf("here1\n");
     int result = 0;
     char * line = NULL;
     FILE *fp = fopen(RACES_PATH, "r");
@@ -158,24 +114,14 @@ int race_validator(char * race) {
         __FILE__, __LINE__, errno, RACES_PATH, strerror(errno));
         exit(EXIT_FAILURE);
     }
-    printf("here2\n");
     size_t len = 0;
     ssize_t read;
     while ((read = getline(&line, &len, fp)) != -1) {
-        printf("here2.5\n");
-        if (strcmp(line, "race") == 0) {
-            printf("here2.7\n");
+        if (strcmp(strtok(line, "\n"), race) == 0) {
             result++;
         } 
     }
-    /*
-    for (int i = 0; (fgets(line, sizeof line, fp) != NULL); i++) {
-        if (strcmp(line, race) == 0) {
-            result++;
-        } 
-    }*/
     fclose(fp);
-    printf("here3\n");
     return result;
 }
 
@@ -209,7 +155,7 @@ void message_error() {
 
 void message_help() {
     // to define
-}
+} 
 
 void message_npc() {
     // to define
